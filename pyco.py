5bb8de938af8a50664a33bd593b47666306b8c35
Nishant Deshpande <deshpande.nishant12@gmail.com>
2020-10-11 19:11:23 -0400

    included active and closed global cases

--- pyco.py
+++ pyco.py
@@ -3,6 +3,17 @@ import sys
 import requests
 import bs4
 
+headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Max-Age': '3600',
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
+}
+
+ACCEPTED_GLOBAL_PARAMS = ['g', 'global', '-g', '-global']
+ACCEPTED_LOCAL_PARAMS = ['l', 'local', '-l', '-local']
+
 def get_global_infected_count(soup):
     total_count = soup.select('.maincounter-number')
     return total_count[0].text.strip()
@@ -15,14 +26,31 @@ def get_global_recovered_count(soup):
     total_count = soup.select('.maincounter-number')
     return total_count[2].text.strip()
 
+def get_global_active_cases(soup):
+    active_cases = soup.select('.panel_flip .panel_front .number-table-main')
+    return active_cases[0].text.strip()
+
+def get_global_closed_cases(soup):
+    closed_cases = soup.select('.panel_flip .panel_front .number-table-main')
+    return closed_cases[1].text.strip()
+
+def get_local_page():
+    pass
+
 def main():
     res = requests.get('https://www.worldometers.info/coronavirus/')
     res.raise_for_status()
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
+
     infected_count = get_global_infected_count(soup)
     death_count = get_global_deaths_count(soup)
     recovered_count = get_global_recovered_count(soup)
-    print('Total infected cases: {0} | Total deaths: {1} | Total recovered: {2}'.format(infected_count, death_count, recovered_count))
+    active_cases = get_global_active_cases(soup)
+    closed_cases = get_global_closed_cases(soup)
+
+    if (len(sys.argv) == 1) or (len(sys.argv) == 2 and sys.argv[1] in ACCEPTED_GLOBAL_PARAMS):
+        print('Total infected cases: {0} | Total deaths: {1} | Total recovered: {2}'.format(infected_count, death_count, recovered_count))
+        print('Active cases: {0} | Closed cases: {1}'.format(active_cases, closed_cases))
 
 if __name__ == '__main__':
     main()
