24371673d8b9de902180628cf240dcaf8c271444
Nishant Deshpande <deshpande.nishant12@gmail.com>
2020-10-11 18:43:57 -0400

    init global count

--- pyco.py
+++ pyco.py
@@ -3,9 +3,15 @@ import sys
 import requests
 import bs4
 
+def get_global_count(soup):
+    total_count = soup.select('.maincounter-number')
+    return total_count[0].text.strip()
+
 def main():
     res = requests.get('https://www.worldometers.info/coronavirus/')
     res.raise_for_status()
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
-    total_count = soup.select('#maincounter-wrap .maincounter-number')
-    print(len(total_count))
+    print(get_global_count(soup))
+
+if __name__ == '__main__':
+    main()
