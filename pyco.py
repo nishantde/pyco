2a72f46966af7f6b878749f824f01ac816e55169
Nishant Deshpande <deshpande.nishant12@gmail.com>
2020-10-11 20:00:52 -0400

    added local case info retrieval

--- pyco.py
+++ pyco.py
@@ -48,6 +48,17 @@ def get_local_recovered_count(local_case_info):
     '''Returns state-wise total recovery count'''
     return local_case_info[2].text.strip()
 
+def display_cases(infected, deaths, recovered):
+    '''Displays the infected count, death count, and recovery count'''
+    print('Total infected:', infected)
+    print('Total deaths:', deaths)
+    print('Total recovered:', recovered)
+
+def display_active_cases(active, closed):
+    '''Displays active case count and closed case count'''
+    print('Current active cases:', active)
+    print('Closed cases:', closed)
+
 def main():
     res = requests.get(URL, headers=headers)
     res.raise_for_status()
@@ -63,29 +74,38 @@ def main():
     active_cases = get_global_active_cases(global_active_case_info)
     closed_cases = get_global_closed_cases(global_active_case_info)
 
-    # Retrieving local info
-    g = geocoder.ip('me')
-    city, state, country = g.city, g.state, g.country
-    STATE_NAME = state
-    COUNTRY_NAME = country
-    
-    # Handling discrepancy between nomenclature
-    if country == 'US': 
-        country = 'USA'
-
-    # Resolving URL for retrieving local info
-    state = '-'.join(name for name in state.split())
-    local_url = URL + '/' + country.lower() + '/' + state.lower() + '/'
-    local_res = requests.get(local_url, headers=headers)
-    local_res.raise_for_status()
-    local = bs4.BeautifulSoup(local_res.text, 'html.parser')
-
-    local_case_info = local.select('#maincounter-wrap .maincounter-number')
-
+    # Retrieve global info if no parameters are given or acceptable global parameters are given
     if (len(sys.argv) == 1) or (len(sys.argv) == 2 and sys.argv[1] in ACCEPTED_GLOBAL_PARAMS):
-        print('Total infected cases: {0} | Total deaths: {1} | Total recovered: {2}'.format(infected_count, death_count, recovered_count))
-        print('Active cases: {0} | Closed cases: {1}'.format(active_cases, closed_cases))
+        print('Global cases')
+        display_cases(infected_count, death_count, recovered_count)
+        display_active_cases(active_cases, closed_cases)
 
+    if len(sys.argv) == 2:
+        if sys.argv[1] in ACCEPTED_LOCAL_PARAMS:
+            # Retrieving local info
+            g = geocoder.ip('me')
+            city, state, country = g.city, g.state, g.country
+            STATE_NAME, COUNTRY_NAME = state, country
+            
+            # Handling discrepancy between nomenclature
+            if country == 'US': 
+                country = 'USA'
+
+            # Resolving URL for retrieving local info
+            state = '-'.join(name for name in state.split())
+            local_url = URL + '/' + country.lower() + '/' + state.lower() + '/'
+            local_res = requests.get(local_url, headers=headers)
+            local_res.raise_for_status()
+            local = bs4.BeautifulSoup(local_res.text, 'html.parser')
+
+            local_case_info = local.select('#maincounter-wrap .maincounter-number')
+            
+            local_infected_count = get_local_infected_count(local_case_info)
+            local_death_count = get_local_death_count(local_case_info)
+            local_recovered_count = get_local_recovered_count(local_case_info)
+
+            print('State: {0} & Country: {1}'.format(STATE_NAME, COUNTRY_NAME))
+            display_cases(local_infected_count, local_death_count, local_recovered_count)
 
 if __name__ == '__main__':
     main()
